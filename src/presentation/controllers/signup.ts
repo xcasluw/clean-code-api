a83180f2dee2293e9f63782a5c445b7f4dfc96b1
Lucas <oligena@hotmail.com>
2022-04-28 18:15:46 -0300

    return 400 if no password confirmation is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -10,5 +10,11 @@ export class SignUpController {
     if (!httpRequest.body.email) {
       return badRequest(new MissingParamError('email'))
     }
+    if (!httpRequest.body.password) {
+      return badRequest(new MissingParamError('password'))
+    }
+    if (!httpRequest.body.passwordConfirmation) {
+      return badRequest(new MissingParamError('passwordConfirmation'))
+    }
   }
 }
