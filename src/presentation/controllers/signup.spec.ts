1b7496cd94c46d9eb4b3365247f642fc8dbbe1bd
Lucas <oligena@hotmail.com>
2022-04-29 10:39:21 -0300

    refator(*): move all protocols to a single file

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,7 +1,7 @@
 import { SignUpController } from './signup'
 import { MissingParamError, InvalidParamError } from '../errors'
 import { ServerError } from '../errors/server-error'
-import { EmailValidator } from '../protocols/email-validator'
+import { EmailValidator } from '../protocols'
 
 interface SutTypes {
   sut: SignUpController
